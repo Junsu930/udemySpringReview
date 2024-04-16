927f135c6c9a21ab40f6ab63f9993dea9929698a
junsu930 <bobdylan0331@gmail.com>
2024-04-16 15:26:58 +0900

    new wepapp

--- learn-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jpa/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jpa/CourseCommandLineRunner.java
@@ -7,7 +7,7 @@ import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.Cour
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
+import java.util.*;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
@@ -27,5 +27,6 @@ public class CourseCommandLineRunner implements CommandLineRunner {
         System.out.println(repository.count());
         System.out.println(repository.findByAuthor("Sehee"));
         System.out.println(repository.findByName("Learn Trumpet"));
+
     }
 }
