3aacdfa7d8fdcc0ece513b56ee2272963a85beca
junsu930 <bobdylan0331@gmail.com>
2024-06-13 18:08:47 +0900

    rest api

---
+++ restful-web-services/restful-web-services/src/main/java/com/junsu/rest/webservice/restful_web_services/versioning/PersonV2.java
@@ -0,0 +1,12 @@
+package com.junsu.rest.webservice.restful_web_services.versioning;
+
+public class PersonV2 {
+    private Name name;
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
